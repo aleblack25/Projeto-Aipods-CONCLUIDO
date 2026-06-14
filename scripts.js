6080055c5b4525e2eef42aebd8efd185713cbb31
aleblack25 <alexandreacostacosta@gmail.com>
2026-06-14 18:51:03 -0300

    Projeto Criativo Com Animação JavaScript

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const iten = document.querySelectorAll('.iten')
+const dots = document.querySelectorAll('.dot')
+const numberIndicators = document.querySelector('.numbers')
+const list = document.querySelector('.list')
+
+let active = 0
+const total = iten.length
+let timer;
+
+
+function update(direction) {
+    
+   document.querySelector('.iten.active').classList.remove('active')
+   document.querySelector('.dot.active').classList.remove('active')
+
+
+
+ if(direction > 0){
+     active = active + 1
+       
+     if(active===total){
+           active = 0
+     }
+ }
+
+   else if (direction < 0){
+         
+       active = active - 1
+       
+       if(active < 0){
+         active = total -1
+       }
+
+       
+}
+
+
+ 
+
+   iten[active].classList.add('active')   
+   dots[active].classList.add('active')
+
+   numberIndicators.textContent = String(active +1 ).padStart(2,'0')
+
+ }
+
+ clearInterval(timer)
+  timer = setInterval(() => {
+   update(1)
+  }, 5000);
+
+
+
+
+
+ prevButton.addEventListener('click', () => {
+   update(-1)
+
+ })
+
+ nextButton.addEventListener('click', () => {
+   update(1)
+   
+ })
